6b70ed9ca5efbbf423198db58992cd421bb3be19
tungduong <duongphan300@gmail.com>
2025-01-16 21:04:41 +0700

    push

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Collection from "./pages/Collection";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import PlaceOrder from "./pages/PlaceOrder";
+import Orders from "./pages/Orders";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Product/:ProductId" element={<Product />} />
+        <Route path="/Cart" element={<Cart />} />
+        <Route path="/PlaceOrder" element={<PlaceOrder />} />
+        <Route path="/Orders" element={<Orders />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
